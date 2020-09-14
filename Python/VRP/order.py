c150870fb399ee1a28d39557dd6faf8dfb3bcd5e
junbjnnn <23205674+junbjnnn@users.noreply.github.com>
2020-09-15 00:07:33 +0700

    Create order.py

---
+++ Python/VRP/order.py
@@ -0,0 +1,27 @@
+import math
+
+
+class Order:
+    def __init__(self, idd, address, x, y, delivery_amount, pickup_amount, distancee=None,
+                 distance_to_stores=None, is_return=False):
+        self.idd = idd
+        self.address = address
+        self.x = x
+        self.y = y
+        self.delivery_amount = 0 if math.isnan(delivery_amount) else delivery_amount
+        self.pickup_amount = 0 if math.isnan(pickup_amount) else pickup_amount
+
+        # Distance from order or depot to all remaining orders
+        if distancee is None:
+            distancee = []
+        self.distancee = distancee
+
+        # Distance from order or depot to all remaining orders
+        if distance_to_stores is None:
+            distance_to_stores = []
+        self.distance_to_stores = distance_to_stores
+
+        self.is_return = is_return
+
+    def __repr__(self):
+        return str(self.__dict__)
