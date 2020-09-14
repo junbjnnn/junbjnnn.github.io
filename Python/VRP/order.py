14a3f51380a600e7f854ecff5db1c70debb6e5a3
junbjnnn <23205674+junbjnnn@users.noreply.github.com>
2020-09-15 00:08:29 +0700

    Update order.py

--- Python/VRP/order.py
+++ Python/VRP/order.py
@@ -3,7 +3,7 @@ import math
 
 class Order:
     def __init__(self, idd, address, x, y, delivery_amount, pickup_amount, distancee=None,
-                 distance_to_stores=None, is_return=False):
+                 distance_to_stores=None, prob_return = 0, is_return=False):
         self.idd = idd
         self.address = address
         self.x = x
@@ -21,6 +21,7 @@ class Order:
             distance_to_stores = []
         self.distance_to_stores = distance_to_stores
 
+        self.prob_return = prob_return
         self.is_return = is_return
 
     def __repr__(self):
